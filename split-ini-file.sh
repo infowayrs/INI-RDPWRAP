e02629cfed09e6eec66b9f38b220c5c78bc1f57c
Andrew McGlashan <andrew.mcglashan@affinityvision.com.au>
2020-01-28 03:39:07 +1100

    Fixed file type format (dos), other slight adjustments  - ensure only ONE blank line after each INI section

--- split-ini-file.sh
+++ split-ini-file.sh
@@ -22,21 +22,27 @@ case "${BN}" in
 		rm -f -- \[*
 
 		# split INI file sections to individual files
+		# - convert all output files to unix file format
+		# - also squeeze blank lines
+		cat "${SRC_FILE}"|tr -d ''|cat -s|
 		/usr/bin/awk \
 			'/^\[/ { ofn=$1 }
 				ofn {
 						print > ofn
 					   	system("sleep 0.0000000001")
-					}' "${SRC_FILE}"
+					}'
 
-		# remove \r from filenames if created above with them
-		for filex in \[*
-		do
-			filex2=$(echo "${filex}" | tr -d '\r')
-			[[ "${filex}" = "${filex2}" ]] || {
-				do_cmd mv -v "${filex}" "${filex2}"
-			}
-		done
+#
+# This should NOT be necessary
+#
+#		# remove  from filenames if created above with them
+#		for filex in \[*
+#		do
+#			filex2=$(echo "${filex}" | tr -d '')
+#			[[ "${filex}" = "${filex2}" ]] || {
+#				do_cmd mv -v "${filex}" "${filex2}"
+#			}
+#		done
 		;;
 	rebuild-ini-file.sh)
 		# reconstruct INI file
@@ -50,17 +56,22 @@ case "${BN}" in
 		for x in ./\[[MPS]*
 		do
 			cat "${x}"
+			echo
 		done
 
 		# the following "ls" is to ensure ordering of INI sections...
 		for x in $(ls -av -- ./\[[0-9]*)
 		do
 			cat "${x}"
+			echo
 		done
-		)| tr -d '' > ./"${SRC_FILE}".new
+		)| tr -d '' |cat -s > ./"${SRC_FILE}".new
 
 		# update "Updated=" entry
 		sed -i 's/Updated=.*$/Updated='"$(date +%Y-%m-%d)"'/' "${SRC_FILE}".new
+
+		# convert "${SRC_FILE}".new to ff=dos
+		sed -i 's/$/\r/' ./"${SRC_FILE}".new
 
 		# cleanup
 		rm -- ./\[[MPS]*
