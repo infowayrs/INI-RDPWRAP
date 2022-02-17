cce438f9568821315d466e1f08f3b748fa2a85a5
Andrew McGlashan <andrew.mcglashan@affinityvision.com.au>
2022-02-17 20:21:22 +1100

    refactor split and build script  - now includes enable alpha versions as well

--- split-ini-file.sh
+++ split-ini-file.sh
@@ -1,81 +1,92 @@
 #!/bin/bash
 
-do_cmd () {
+split_ini () {
+	# remove any formerly existing INI file sections in this dir
+	rm -f -- \[*
 
-	echo -e "${@}"
-	date
-	eval "${@}"
-	CMD_STATUS=$?
-	echo -e "\n -- status: ${CMD_STATUS}\n\n"
-	date
-	return "${CMD_STATUS}"
-
+	# split INI file sections to individual files
+	# - convert all output files to unix file format
+	# - also squeeze blank lines
+	WRK_SRC_FILE=$(mktemp)
+	tr -d '' < "${SRC_FILE}" > "${WRK_SRC_FILE}"
+	/usr/bin/awk \
+		'/^\[/ { ofn=$1 }
+			ofn {
+					print > ofn
+				}' "${WRK_SRC_FILE}"
+	[[ -f ${WRK_SRC_FILE} ]] && rm "${WRK_SRC_FILE}"
 }
 
+rebuild_ini () {
+	# new version will end in alpha char, a through j
+	# - if it was just a date without alpha version then start with 'a'
+	#   dtherwise increment alphabetically up to "j"
+	DATEX=$(date +%Y-%m-%d)
+	LAST_UPDATED_LINE=$(grep ^Updated= rdpwrap.ini|tr -d '\r')
+	LAST_UPDATED_LINE_VER=${LAST_UPDATED_LINE:(-1)}
+	VER=a
+	NEW_UPDATED_LINE="Updated=${DATEX}${VER}"
+	[[ ${LAST_UPDATED_LINE_VER} = [[:digit:]] ]] || {
+		# Last updated line should have alpha version for date
+		if [[ ${LAST_UPDATED_LINE_VER} = [[:alpha:]] ]]
+		then
+			# increment alphabetic ver
+			VER=$(echo "${LAST_UPDATED_LINE_VER}"|tr "a-j" "b-k")
+			[[ "${VER}" = "k" ]] && {
+				echo doing nothing as too many versions already...
+				exit 2
+			}
+		else
+			echo bad version char is not alphabetic
+			exit 2
+		fi
+		NEW_UPDATED_LINE="Updated=${DATEX}${VER}"
+	}
+	# reconstruct INI file
+	mapfile -t INI_SECTIONS < <(
+		find . -maxdepth 1 -type f -name '\[[0-9]*'|sed 's!^./!!'|sort -V
+	)
+	[[ ${#INI_SECTIONS[@]} -eq 0 ]] && {
+		echo
+		echo No INI sections found... quitting
+		echo
+		exit 2
+	}
+	(
+	cat <<-EOF
+		; RDP Wrapper Library configuration
+		; Do not modify without special knowledge
 
-BN=$(basename "${0}")
+		$(cat \[[MPS]*)
+
+		$(cat "${INI_SECTIONS[@]}")
+
+	EOF
+	) |cat -s > ./"${SRC_FILE}".new
+
+	# update "Updated=" entry
+	sed -i "s/Updated=.*$/${NEW_UPDATED_LINE}/" "${SRC_FILE}".new
+
+	# convert "${SRC_FILE}".new to ff=dos
+	sed -i 's/$/\r/' ./"${SRC_FILE}".new
+
+	# cleanup
+	rm -- ./\[[MPS]*
+	rm -- ./\[[0-9]*
+}
+
 SRC_FILE=./rdpwrap.ini
 
-case "${BN}" in
-	split-ini-file.sh)
-		# remove any formerly existing INI file sections
-		rm -f -- \[*
-
-		# split INI file sections to individual files
-		# - convert all output files to unix file format
-		# - also squeeze blank lines
-		cat "${SRC_FILE}"|tr -d ''|cat -s|
-		/usr/bin/awk \
-			'/^\[/ { ofn=$1 }
-				ofn {
-						print > ofn
-					   	system("sleep 0.0000000001")
-					}'
-
-#
-# This should NOT be necessary
-#
-#		# remove  from filenames if created above with them
-#		for filex in \[*
-#		do
-#			filex2=$(echo "${filex}" | tr -d '')
-#			[[ "${filex}" = "${filex2}" ]] || {
-#				do_cmd mv -v "${filex}" "${filex2}"
-#			}
-#		done
-		;;
-	rebuild-ini-file.sh)
-		# reconstruct INI file
-		(
-		cat <<-EOF
-			; RDP Wrapper Library configuration
-			; Do not modify without special knowledge
-
-		EOF
-
-		for x in ./\[[MPS]*
-		do
-			cat "${x}"
-			echo
-		done
-
-		# the following "ls" is to ensure ordering of INI sections...
-		for x in $(ls -av -- ./\[[0-9]*)
-		do
-			cat "${x}"
-			echo
-		done
-		)| tr -d '' |cat -s > ./"${SRC_FILE}".new
-
-		# update "Updated=" entry
-		sed -i 's/Updated=.*$/Updated='"$(date +%Y-%m-%d)"'/' "${SRC_FILE}".new
-
-		# convert "${SRC_FILE}".new to ff=dos
-		sed -i 's/$/\r/' ./"${SRC_FILE}".new
-
-		# cleanup
-		rm -- ./\[[MPS]*
-		rm -- ./\[[0-9]*
+case "${0##*/}" in
+	split-ini-file.sh) split_ini ;;
+	rebuild-ini-file.sh) rebuild_ini ;;
+	*) echo "No work, incorrect filename...."
+		exit 2
 		;;
 esac
 
+[[ -f ${SRC_FILE} && -f ${SRC_FILE}.new ]] && {
+	diff -b --color "${SRC_FILE}" "${SRC_FILE}.new"
+}
+
+exit 0
